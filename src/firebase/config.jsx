576bb8653d77bb9bdb40f3fd876263ba29e41b24
Teena Verma <teena21072004@gmail.com>
2026-09-01 23:41:41 +0530

    Feat: improve firebase security rules

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA6vomAGiWgtl1ix8xnZ6G9ELuk36-Owhc",
-  authDomain: "d-light-cdb1c.firebaseapp.com",
-  projectId: "d-light-cdb1c",
-  storageBucket: "d-light-cdb1c.firebasestorage.app",
-  messagingSenderId: "136915027694",
-  appId: "1:136915027694:web:9eb839f67ad7bd6d85d5ff",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
